1bc18c05c03dd6badb25e444a16fdd7679176086
passosleo <leonardopassos98@gmail.com>
2022-10-17 01:30:11 -0300

    update netcore

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
@@ -30,6 +31,97 @@ namespace Api.Application.Controllers
             try
             {
                 return Ok(await _service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetWithId")]
+        public async Task<ActionResult> Get(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.Get(id));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] UserEntity user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Post(user);
+
+                if(result != null)
+                {
+                    return Created(new Uri(Url.Link("GetWithId", new {id = result.Id})),result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] UserEntity user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Put(user);
+
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return  Ok(await _service.Delete(id));
             }
             catch (ArgumentException e)
             {
--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -4,9 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Api.CrossCutting.DependencyInjection;
 using AutoMapper;
-//using Api.Data.Context;
-//using Api.Domain.Security;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -17,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace Application
@@ -36,11 +34,12 @@ namespace Application
             ConfigureService.ConfigureDependenciesService(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
-
+            services.AddControllers();
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1",
-                    new Info
+                    new OpenApiInfo
                     {
                         Title = "API DDD",
                         Version = "v1",
@@ -51,63 +50,7 @@ namespace Application
                         //    Url = "https://github.com/mfrinfo"
                         //}
                     });
-
-                //Colocar JWT no Swagger
-                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
-                {
-                    In = "header",
-                    Description = "Entre com o Token JWT",
-                    Name = "Authorization",
-                    Type = "apiKey"
-                });
-
-                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>> {
-                   { "Bearer", Enumerable.Empty<string>() },
-                });
             });
-
-            //var signingConfigurations = new SigningConfigurations();
-            //services.AddSingleton(signingConfigurations); //Instancia Unica
-
-            //var tokenConfigurations = new TokenConfigurations();
-            //new ConfigureFromConfigurationOptions<TokenConfigurations>(
-            //Configuration.GetSection("TokenConfigurations"))
-            //.Configure(tokenConfigurations);
-            //services.AddSingleton(tokenConfigurations);  //Instancia Unica
-
-            //services.AddAuthentication(authOptions =>
-            //{
-            //    authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            //    authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            //}).AddJwtBearer(bearerOptions =>
-            //{
-            //    var paramsValidation = bearerOptions.TokenValidationParameters;
-            //    //paramsValidation.IssuerSigningKey = signingConfigurations.Key;
-            //    //paramsValidation.ValidAudience = tokenConfigurations.Audience;
-            //    //paramsValidation.ValidIssuer = tokenConfigurations.Issuer;
-
-            //    // Valida a assinatura de um token recebido
-            //    paramsValidation.ValidateIssuerSigningKey = true;
-
-            //    // Verifica se um token recebido ainda é válido
-            //    paramsValidation.ValidateLifetime = true;
-
-            //    // Tempo de tolerância para a expiração de um token (utilizado
-            //    // caso haja problemas de sincronismo de horário entre diferentes
-            //    // computadores envolvidos no processo de comunicação)
-            //    paramsValidation.ClockSkew = TimeSpan.Zero;
-            //});
-
-            // Ativa o uso do token como forma de autorizar o acesso
-            // a recursos deste projeto
-            //services.AddAuthorization(auth =>
-            //{
-            //    auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
-            //        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme‌​)
-            //        .RequireAuthenticatedUser().Build());
-            //});
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -120,10 +63,11 @@ namespace Application
 
             // Ativando middlewares para uso do Swagger
             app.UseSwagger();
+
             app.UseSwaggerUI(c =>
             {
-                c.RoutePrefix = string.Empty;
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso de API com AspNetCore 2.2");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso de API com AspNetCore 3.1");
+                c.RoutePrefix = "swagger";
             });
 
             //Redireciona o Link para o Swagger, quando acessar a rota principal
@@ -131,7 +75,14 @@ namespace Application
             option.AddRedirect("^$", "swagger");
             app.UseRewriter(option);
 
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseEndpoints(
+                endpoints =>
+                {
+                    endpoints.MapControllers();
+                }
+            );
         }
     }
 }
